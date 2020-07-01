69c06203537e7e3ff131f0b649aa4f8716c4d991
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-07-01 20:29:51 +0800

    remove redundant library call

--- bmi-calculator/lib/screens/results_page.dart
+++ bmi-calculator/lib/screens/results_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:bmi_calculator/constants.dart';
 import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:bmi_calculator/components/bottom_button.dart';
-import 'package:bmi_calculator/calculator_brain.dart';
 
 class ResultsPage extends StatelessWidget {
   final String bmiResult;
